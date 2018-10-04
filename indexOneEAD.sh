714ef7699f29e2a8f1dd069df69b670fa5f033e7
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2018-10-04 12:17:51 -0400

    indexing script was just for testing

--- indexOneEAD.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-TYPE="$1"
-
-
-AREA=${TYPE//[0-9.]/}
-echo $AREA/$TYPE.xml
-
-bundle exec rake arclight:index FILE=../collections/$AREA/$TYPE.xml REPOSITORY_ID=$AREA SOLR_URL=https://solr.library.albany.edu:8984/solr/arclight-testing
