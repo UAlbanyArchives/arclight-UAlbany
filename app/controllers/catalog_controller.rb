676fc8d28b5cc98f43188dabc92c7ffbb22ee9df
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2021-03-04 10:19:16 -0500

    display and css fixes

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -234,9 +234,9 @@ class CatalogController < ApplicationController
     ]
 
     config.show.context_access_tab_items = %i[
+      in_person_field
       terms_field
       cite_field
-      in_person_field
       contact_field
     ]
 
@@ -274,7 +274,8 @@ class CatalogController < ApplicationController
     config.add_background_field 'accruals_ssm', label: 'Accruals', helper_method: :render_html_tags
     config.add_background_field 'phystech_ssm', label: 'Physical / technical requirements', helper_method: :render_html_tags
     config.add_background_field 'physloc_ssm', label: 'Physical location', helper_method: :render_html_tags
-    config.add_background_field 'descrules_ssm', label: 'Rules or conventions', helper_method: :render_html_tags
+    config.add_background_field 'odd_ssm', label: 'Other', helper_method: :render_html_tags
+    #config.add_background_field 'descrules_ssm', label: 'Rules or conventions', helper_method: :render_html_tags
 
     # Collection Show Page - Related Section
     config.add_related_field 'relatedmaterial_ssm', label: 'Related material', helper_method: :render_html_tags
@@ -349,6 +350,10 @@ class CatalogController < ApplicationController
     # ACCESS TAB FIELDS
     # =================
 
+    # Collection and Component Show Page Access Tab - In Person Section
+    #config.add_in_person_field 'repository_ssm', if: :repository_config_present, label: 'Location of this collection', helper_method: :context_access_tab_repository
+    config.add_in_person_field 'id', if: :before_you_visit_note_present, label: 'Access', helper_method: :context_access_tab_visit_note # Using ID because we know it will always exist
+
     # Collection Show Page Access Tab - Terms and Conditions Section
     config.add_terms_field 'accessrestrict_ssm', label: 'Restrictions', helper_method: :render_html_tags
     config.add_terms_field 'userestrict_ssm', label: 'Terms of Access', helper_method: :render_html_tags
@@ -359,15 +364,11 @@ class CatalogController < ApplicationController
     config.add_component_terms_field 'parent_access_restrict_ssm', label: 'Parent Restrictions', helper_method: :render_html_tags
     config.add_component_terms_field 'parent_access_terms_ssm', label: 'Parent Terms of Access', helper_method: :render_html_tags
 
-    # Collection and Component Show Page Access Tab - In Person Section
-    config.add_in_person_field 'repository_ssm', if: :repository_config_present, label: 'Location of this collection', helper_method: :context_access_tab_repository
-    config.add_in_person_field 'id', if: :before_you_visit_note_present, label: 'Before you visit', helper_method: :context_access_tab_visit_note # Using ID because we know it will always exist
-
     # Collection and Component Show Page Access Tab - How to Cite Section
     config.add_cite_field 'prefercite_ssm', label: 'Preferred citation', helper_method: :render_html_tags
 
     # Collection and Component Show Page Access Tab - Contact Section
-    config.add_contact_field 'repository_ssm', if: :repository_config_present, label: 'Contact', helper_method: :access_repository_contact
+    #config.add_contact_field 'repository_ssm', if: :repository_config_present, label: 'Contact', helper_method: :access_repository_contact
 
     # Remove unused show document actions
     %i[citation email sms].each do |action|
