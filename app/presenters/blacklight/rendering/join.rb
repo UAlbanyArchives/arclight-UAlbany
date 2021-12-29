aac4bb64f8c85351aa2815d29e87474b0c7964b8
Greg Wiedeman <gregory.wiedeman1@gmail.com>
2021-12-29 15:00:25 -0500

    minor display bugfixes

---
+++ app/presenters/blacklight/rendering/join.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Blacklight
+  module Rendering
+    class Join < AbstractStep
+      def render
+        options = config.separator_options || {}
+        next_step(values.map { |x| html_escape(x) })
+      end
+
+      private
+
+      def html_escape(*args)
+        ERB::Util.html_escape(*args)
+      end
+    end
+  end
+end
+
---
+++ indexEADs.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+TYPE="$1"
+
+[ -s "/home/railsdev/.rvm/scripts/rvm" ] && . "/home/railsdev/.rvm/scripts/rvm"
+
+export REPOSITORY_ID=$TYPE
+
+rvm 2.6.5@arclight-test
+
+echo "$(date) $line Export $TYPE" #>> /media/SPE/indexing-logs/index.log
+cd /var/www/update/arclight-UAlbany
+
+find /opt/lib/collections/$TYPE -type f -name '*.xml' -exec \
+bundle exec traject -u https://solr2020.library.albany.edu:8984/solr/arclight-fixed -i xml -c /home/railsdev/.rvm/gems/ruby-2.6.5@arclight-test/bundler/gems/arclight-f9b61c2cf12c/lib/arclight/traject/ead2_config.rb {} ';'
+
+
