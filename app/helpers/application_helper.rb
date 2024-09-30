eaab58e8685f89f270c624099c1dc020d30b75d8
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2024-09-30 12:18:01 -0400

    moved arclight search bar to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,16 @@ module ApplicationHelper
     'Archives & Manuscripts'
   end
 
+  # search bar is custom to arclight so we need a helper
+  def render_search_bar(params: {}, q: nil, search_field: nil)
+    params ||= {}
+    render(Arclight::SearchBarComponent.new(
+      url: search_catalog_path,
+      params: params.merge(f: (params[:f] || {}).except(:collection)),
+      q: q,
+      search_field: search_field,
+      autocomplete_path: suggest_index_catalog_path
+    ))
+  end
+
 end
