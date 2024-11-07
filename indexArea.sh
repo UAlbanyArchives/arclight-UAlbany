61465bfc1176348cc1f384b56dc33e02468b73da
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2024-11-07 13:49:24 -0500

    now checks bundle before indexing

--- indexArea.sh
+++ indexArea.sh
@@ -10,4 +10,6 @@ URL='https://solr2020.library.albany.edu:8984/solr/arclight-1.4'
 #CONFIG='/usr/local/bundle/gems/arclight-1.4.0/lib/arclight/traject/ead2_config.rb'
 CONFIG='lib/arclight/traject/ead2_config.rb'
 
+bundle check || bundle install
+
 REPOSITORY_ID=$TYPE find /ead/$TYPE -type f -name '*.xml' -exec env REPOSITORY_ID=$TYPE bundle exec traject -u $URL -i xml -c $CONFIG {} ';'
