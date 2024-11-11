6137d72bed00b037726c737451322ff480cd4f3d
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2024-11-11 12:24:14 -0500

    fixed restriction coloring

--- app/components/arclight/access_component.rb
+++ app/components/arclight/access_component.rb
@@ -20,6 +20,23 @@ module Arclight
       Array(component_access_items)
     end
 
+    def restriction_classes
+      access_restriction = presenter.document["accessrestrict_tesim"].to_s.downcase
+
+      if presenter.document.level == "collection"
+        return "" if access_restriction.include?("access to this collection is unrestricted") ||
+                    access_restriction.include?("access to these records is unrestricted") ||
+                    access_restriction.include?("this collection is unrestricted") ||
+                    access_restriction.include?("access to this record group is unrestricted")
+        
+        return "accessrestrict-danger" if access_restriction.blank?
+        
+        "accessrestrict-warning"
+      else
+        access_restriction.blank? ? nil : "accessrestrict-danger"
+      end
+    end
+
     delegate :collection_access_items, :component_access_items, to: :show_config
   end
 end
